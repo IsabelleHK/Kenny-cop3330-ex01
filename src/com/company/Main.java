3706b2bfce57fe0836103a65be348ff714ef19df
Isabelle <ihkenny01@gmail.com>
2021-09-12 19:25:12 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+/*
+    UCF COP3330 Fall 2021 Assignment 1 Solution
+    Copyright 2021 Isabelle Kenny
+ */
+
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+
+        String userName = userInput.nextLine();
+        System.out.println("Hello, " + userName + ", nice to meet you!");
+    }
+}
